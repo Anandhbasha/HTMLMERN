d2dfbbefb38eab6285ac10f359bfa10d9e78336d
Anandhbasha <anadhakm@gmail.com>
2025-07-08 11:40:09 +0530

    js

---
+++ main.js
@@ -0,0 +1,23 @@
+// Hoisting
+    //variable declaration
+        //var
+        console.log(a);
+        var a = 10
+        //start with letter
+        // dont use space and -
+        //meaning full names
+        // dont use js names for varible dec
+        var a =20
+        
+        
+        // let
+        // console.log(b);
+        let b;
+    console.log(b);
+        
+        
+        //const
+        const c =60;
+        c=80
+        console.log(c);
+        
