6f1264ac46e5e69ad7fe50ad8e419430be9c84d7
Anandhbasha <anadhakm@gmail.com>
2025-07-11 11:26:37 +0530

    functions

--- main.js
+++ main.js
@@ -37,64 +37,167 @@
 //function scope or Global scope
 // function add(){
 //     let b=20
-//     {
-//         console.log(b);
+// //     {
+// //         console.log(b);
         
-//     }
-// }
-// add()
+// //     }
+// // }
+// // add()
 
-//data types
-    // premitive data types
-        // number
-            var num = 50.60
-            console.log(typeof(num));
+// //data types
+//     // premitive data types
+//         // number
+//             var num = 50.60
+//             console.log(typeof(num));
             
-        // string
-            let name = "xyz"
-            console.log(typeof(name));
+//         // string
+//             let name = "xyz"
+//             console.log(typeof(name));
             
-        // boolean
-        isAlive = true
-        console.log(typeof(isAlive));
+//         // boolean
+//         isAlive = true
+//         console.log(typeof(isAlive));
         
-        // undefined
-        let c
-        console.log(typeof(c));
+//         // undefined
+//         let c
+//         console.log(typeof(c));
         
-        // Null
-        let d=null
-        console.log(typeof(d));
+//         // Null
+//         let d=null
+//         console.log(typeof(d));
         
-        // Nan - Not an Number
+//         // Nan - Not an Number
     
-        // non premitive
-            //array - index of array
-            let arr = [10,"abc",true,undefined]
-            console.log(typeof(arr));
-            console.log(arr);
-            console.log(arr[0]);
-            console.log(arr[1]);
-            console.log(arr[2]);
-            console.log(arr[3]);
+//         // non premitive
+//             //array - index of array
+//             let arr = [10,"abc",true,undefined]
+//             console.log(typeof(arr));
+//             console.log(arr);
+//             console.log(arr[0]);
+//             console.log(arr[1]);
+//             console.log(arr[2]);
+//             console.log(arr[3]);
 
-            console.log(arr.length);
+//             console.log(arr.length);
             
             
-            // object - Key of Object
-            person = {
-                name:"abc",
-                age:20,
-                family:{
-                    father:"def",
-                    mother:"hjgj",
-                    siblings:{
-                        brother:"trfv",
-                        sister:"hghfhg"
-                    }
-                }
+//             // object - Key of Object
+//             person = {
+//                 name:"abc",
+//                 age:20,
+//                 family:{
+//                     father:"def",
+//                     mother:"hjgj",
+//                     siblings:{
+//                         brother:"trfv",
+//                         sister:"hghfhg"
+//                     }
+//                 }
+//             }
+//             console.log(person);
+//             console.log(person.name);
+//             console.log(person.family.siblings);
+//operators
+    //arithmetic   +,-,*,/,%,++,--
+    let b= 21
+    let a= 40
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a/b);
+    console.log(a%b);
+    // a=a+1
+    console.log(++a); 
+    // a=41
+    console.log(a++);
+    // o a = 41
+    // a=42
+    console.log(a);
+     
+    console.log(--b);
+    console.log(b--);
+    // comparison
+            // ==,===,!= ,<,>,<=,>=
+           
+            console.log(a==b);
+            console.log(a>b);
+            console.log(a<b);
+            console.log(a<=b);
+            console.log(a>=b);
+            console.log(a!=b);
+            let num1 = 10
+            let num2 = "10"
+            console.log(num1==num2);
+            console.log(num1===num2);
+            
+
+            
+
+    // logical
+            // And && OR || 
+        let age = 18
+        let location1 = "TNE"
+       let nationality="Uk"
+        console.log(age>18 && location1=="TN" && nationality=="IN" );
+        console.log(age>18 || location1=="TN" || nationality=="IN" );
+        
+    // assigment operator
+        // =,+=,*=,-=,/=
+        // a=a+b
+        
+        
+        a+=b
+        console.log(a);
+        e=5
+        f=10
+        e*=f
+        console.log(e);
+        
+    // ternary
+    console.log(age>=18?"Adult":"Teenager");
+    
+// function
+    function addtion(num1,num2){
+        return num1+num2        
+    }
+    console.log(addtion(50,44));
+    // console.log(94);
+    
+    
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+    // addtion(50,44)
+
+    // types
+        //arrow
+            const arrow = (c,d)=> {return num1*num2}
+            console.log(arrow(28,88));
+            
+
+            const add = (a,b)=>{
+                return a+b
             }
-            console.log(person);
-            console.log(person.name);
-            console.log(person.family.siblings);
+            console.log(add(10,50));
+            
+        //IIFI
+        const IIFI = (function(g){
+            console.log(g);
+            
+        })
+        IIFI(100)
+        // ananoums
+        const ana = function(){
+            
+        }
+
+// Conditional Statements
+// looping Statements
             
