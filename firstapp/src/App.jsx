aa0239ddd724f0d40a9db5393960e54ffd9f7736
Anandhbasha <anadhakm@gmail.com>
2025-07-31 11:37:39 +0530

    newadd

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,29 +1,96 @@
-import Button from "./Button"
-import Card from "./Card"
-import Shirtimage from './assets/Image/Shirt.jpeg'
+// import Button from "./Button"
+// import Card from "./Card"
+// import Shirtimage from './assets/Image/Shirt.jpeg'
+// import Shirtimage1 from './assets/Image/Shirt.jpeg'
 
-const btn_values = ["click","Login","submit","delete","Update"] 
-const App = ()=>{
-  return(
-    <div className="App" style={{marginLeft:"100px"}}>
-      <h1>Hello welcome to react</h1>
-      <h2>This is Basics of react</h2>
-      {
-        btn_values.map((item)=>(
-          <Button value = {item}/>
-        ))
-      }
-     <div className="card">
-       <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
-      <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
-      <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
-      <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
-      <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
-      <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
-      <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
-     </div>
-    </div>
+// const btn_values = ["click","Login","submit","delete","Update"] 
+// const cardDetails = [
+//   {
+//     image:Shirtimage,
+//     prodName:"Mens Shirt",
+//     price:400,
+//     ProdDes:"Something"
+//   },
+//   {
+//     image:Shirtimage1,
+//     prodName:"Mens Half Sleeve Shirt",
+//     price:400,
+//     ProdDes:"Something"
+//   }
+// ]
+// const App = ()=>{
+//   return(
+//     <div className="App" style={{marginLeft:"100px"}}>
+//       <h1>Hello welcome to react</h1>
+//       <h2>This is Basics of react</h2>
+//       {/* {
+//         btn_values.map((item)=>(
+//           <Button value = {item}/>
+//         ))
+//       } */}
+//      {/* <div className="card">
+//        <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
+//       <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
+//       <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
+//       <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
+//       <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
+//       <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
+//       <Card image = {Shirtimage}  x="Mens Shirt" y="RS:400" des="Lorem New"/>
+//      </div> */}
+//      <div className="card">
+//       {
+//         cardDetails.map((item)=>(
+//           <Card {...item}/>
+//         ))
+//       }
+//      </div>
+//     </div>
     
+//   )
+// }
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ import Card from './Card'
+  const cardDetails = [
+  {
+    image:Shirtimage,
+    prodName:"Mens Shirt",
+    price:400,
+    ProdDes:"Something"
+  },
+  {
+    image:Shirtimage1,
+    prodName:"Mens Half Sleeve Shirt",
+    price:400,
+    ProdDes:"Something",
+    NewFea :"NBHJBKBKb"
+  }
+]
+const App = ()=>{
+
+  return(
+    <div className="Card">
+      {cardDetails.map((item)=>(
+        <Card  {...item}/>
+      ))}
+    </div>
   )
 }
 export default App
--- firstapp/src/Card.jsx
+++ firstapp/src/Card.jsx
@@ -1,14 +1,31 @@
+// import Button from './Button'
+// const Card = ({image,x,y,des})=>{
+//     return(
+//         <div className="cards" >
+//             <div className="topSection">
+//                 <img src={image}></img>
+//             </div>
+//             <div className="bottomSection">
+//                 <h3>{x}</h3>
+//                 <h3>{y}</h3>
+//                 <p>{des}</p>
+//                 <Button value = "add to cart"></Button>
+//             </div>
+//         </div>
+//     )
+// }
+// export default Card
 import Button from './Button'
-const Card = ({image,x,y,des})=>{
+const Card = ({image,prodName,price,ProdDes,NewFea})=>{
     return(
         <div className="cards" >
             <div className="topSection">
                 <img src={image}></img>
             </div>
             <div className="bottomSection">
-                <h3>{x}</h3>
-                <h3>{y}</h3>
-                <p>{des}</p>
+                <h3>{prodName}</h3>
+                <h3>{price}</h3>
+                <p>{ProdDes}</p>
                 <Button value = "add to cart"></Button>
             </div>
         </div>
